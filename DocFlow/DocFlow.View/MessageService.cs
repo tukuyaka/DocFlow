6ab7fa576cdca6490211795589d2743db6c989e4
tukuyaka <tukuyaka@gmail.com>
2020-01-30 14:38:21 +0800

    Добавлена служба сообщений

---
+++ DocFlow/DocFlow.View/MessageService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DocFlow.View
+{
+    public interface IMessageService
+    {
+        void ShowMesage(string message);
+        void ShowExclamation(string message);
+        void ShowError(string message);
+    }
+
+    public class MessageService : IMessageService
+    {
+        public void ShowMesage(string message)
+        {
+            MessageBox.Show(message, "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void ShowExclamation(string message)
+        {
+            MessageBox.Show(message, "Предупреждение", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        public void ShowError(string message)
+        {
+            MessageBox.Show(message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
--- DocFlow/DocFlow/Program.cs
+++ DocFlow/DocFlow/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DocFlow.View;
+using DocFlow.Presenter;
 
 namespace DocFlow
 {
@@ -16,7 +18,7 @@ namespace DocFlow
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form());
+            Application.Run(new BaseForm());
         }
     }
 }
